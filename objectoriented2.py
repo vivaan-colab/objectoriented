8489b86f3334dac8f50a471bfacdea337ec03424
vivaan-colab <vidya89in@gmail.com>
2026-09-05 12:46:59 +0530

    Add files via upload

---
+++ objectoriented2.py
@@ -0,0 +1,10 @@
+class vehicle:
+    def __init__(self,max_speed, mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+modelx=vehicle(240,28)
+modely=vehicle(300,20)
+print(modelx.max_speed)
+print(modelx.mileage)
+print(modely.max_speed)
+print(modely.mileage)
